294eccf9004e561858f575c8ddeeb800ef0545a0
wumeng <wumeng@gupo.onaliyun.com>
2023-06-30 16:46:28 +0800

    更新调用方式

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # middle-office-mc
 古珀对外服务-消息中心（message center）
+
+
+#### 使用
+-	导入本包
+-	在项目根目录的config目录下，新建middleoffice.php文件，内容如下：
+```
+	return [
+		'id' => env('AUTH_CENTER_APP_ID'),
+		'app_id' => env('AUTH_CENTER_APP_KEy'),
+		'app_secret' => env('AUTH_CENTER_APP_SECRET'),
+	];
+```
+-	参数需要去用户中心获取
+		-	AUTH_CENTER_APP_ID（用户中心获取到的应用id）
+		-	AUTH_CENTER_APP_KEY（用户中心获取到的app key）
+		-	AUTH_CENTER_APP_SECRET（用户中心获取到的app secret）
+-	实现MessageCenterClient类，调用方法即可
+```php
+(new MessageCenterClient())->sendMsg($body, $endpoint, $headerExtra);
+```
--- src/App/MessageCenterClient.php
+++ src/App/MessageCenterClient.php
@@ -6,6 +6,7 @@ use Gupo\MiddleOffice\Utils\Utils;
 use Gupo\MiddleOffice\Config\Config;
 use Gupo\MiddleOffice\Clients\Client;
 use Gupo\MiddleOffice\VO\RequestHeader;
+use GuzzleHttp\Exception\GuzzleException;
 use Gupo\MiddleOfficeMc\Error\CenterErrorInfo;
 use Gupo\MiddleOffice\Exception\ClientException;
 use Gupo\MiddleOfficeMc\Exception\CenterException;
@@ -19,80 +20,79 @@ use Gupo\MiddleOfficeMc\Exception\CenterException;
 class MessageCenterClient extends Client
 {
     /**
-     * @param  Config  $config
      * @throws ClientException
      */
-    public function __construct(Config $config)
+    public function __construct()
     {
-        parent::__construct($config);
+        parent::__construct(new Config());
     }
 
     /**
      * 发送消息
      *
      * @param $body
-     * @param $appId
      * @param $endpoint
+     * @param  array  $headerExtra
      * @return mixed
      * @throws ClientException
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws GuzzleException
      * @author Wumeng wumeng@gupo.onaliyun.com
      * @since 2023-06-28 16:58
      */
-    public function sendMsg($body, $appId, $endpoint)
+    public function sendMsg($body, $endpoint, array $headerExtra = [])
     {
-        $header = new RequestHeader($this->config, $body, $appId);
+        $header = new RequestHeader($this->config, $body, $this->config->appId);
 
-        return $this->callApiPost($header->getHeader(), $body, $endpoint . 'openapi/v1/message/send');
+        return $this->callApiPost($header->getHeader($headerExtra), $body, $endpoint . 'openapi/v1/message/send');
     }
 
     /**
      * 查询消息发送状态
      *
      * @param $messageUUID
-     * @param $appId
      * @param $endpoint
-     * @param $extra
+     * @param  array  $extra
+     * @param  array  $headerExtra
      * @return mixed
      * @throws CenterException
      * @throws ClientException
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws GuzzleException
      * @author Wumeng wumeng@gupo.onaliyun.com
      * @since 2023-06-28 17:33
      */
-    public function queryMessageResult($messageUUID, $appId, $endpoint, $extra = [])
+    public function queryMessageResult($messageUUID, $endpoint, array $extra = [], array $headerExtra = [])
     {
         if (Utils::isUnset($messageUUID) || Utils::empty_($messageUUID)) {
             throw new CenterException(CenterErrorInfo::MISSING_MESSAGE_UUID);
         }
         $body = array_merge(['uuid' => $messageUUID], $extra);
-        $header = new RequestHeader($this->config, $body, $appId);
+        $header = new RequestHeader($this->config, $body, $this->config->appId);
 
-        return $this->callApiPost($header->getHeader(), $body, $endpoint . 'openapi/v1/message/sendStatus');
+        return $this->callApiPost($header->getHeader($headerExtra), $body, $endpoint . 'openapi/v1/message/sendStatus');
     }
 
     /**
      * 获取站内信列表
      *
      * @param $channelUserSign
-     * @param $appId
      * @param $endpoint
-     * @param $extra
+     * @param  array  $extra
+     * @param  array  $headerExtra
      * @return mixed
      * @throws CenterException
      * @throws ClientException
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws GuzzleException
      * @author Wumeng wumeng@gupo.onaliyun.com
      * @since 2023-06-28 17:37
      */
-    public function getInternalMsgList($channelUserSign, $appId, $endpoint, $extra = [])
+    public function getInternalMsgList($channelUserSign, $endpoint, array $extra = [], array $headerExtra = [])
     {
         if (Utils::isUnset($channelUserSign) || Utils::empty_($channelUserSign)) {
             throw new CenterException(CenterErrorInfo::MISSING_CHANNEL_USER_SIGN);
         }
         $body = array_merge(['channel_user_sign' => $channelUserSign], $extra);
-        $header = new RequestHeader($this->config, $body, $appId);
+        $header = new RequestHeader($this->config, $body, $this->config->appId);
 
-        return $this->callApiPost($header->getHeader(), $body, $endpoint . 'openapi/v1/message/list');
+        return $this->callApiPost($header->getHeader($headerExtra), $body, $endpoint . 'openapi/v1/message/list');
     }
 }
